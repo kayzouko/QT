55967b03d2bcb0135234d1d442eeeebd02e0b77b
nb <micheljordan-nba@live.fr>
2025-04-08 14:41:11 +0200

    correction du prompt QT

--- backend/utils/ollamaHelper.js
+++ backend/utils/ollamaHelper.js
@@ -10,7 +10,8 @@ async function generateStory(prompt) {
         Crée une histoire en 5 parties séparées par '||'. 
         Chaque partie doit contenir une émotion différente entre []. Les émotions possibles sont :
         joyeux, triste, taquin, En colère, colère, affectueux, Timide, heureux, déçu, chagriné, surpris, surprise, neutre, étonné, fâché, amusé, inquiet. 
-        Exemple: "Le lièvre sautait.[joie]||Il tomba.[triste]"
+        Exemple: "Le lièvre sautait.[joie]||Il tomba.[triste]".
+        Ne dis pas "Voici l'histoire en 5 parties" juste donne l'histoire.
         Thème: ${prompt}
       `,
       stream: false,
